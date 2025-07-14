0c0e7d998d93621dac7284c6dfbcce7f29dcf94a
Gruckus <6582665+gruckus-gruckus@users.noreply.github.com>
2025-07-15 09:41:20 +1000

    make minsdk 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gruckus.tvvolume"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
