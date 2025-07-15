d214bfbd90c947681178adfbbb23b243426ab200
Gruckus <6582665+gruckus-gruckus@users.noreply.github.com>
2025-07-16 08:52:29 +1000

    boot on startup

---
+++ app/src/main/java/com/gruckus/tvvolume/BootReceiver.kt
@@ -0,0 +1,22 @@
+package com.gruckus.tvvolume
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            Log.d("BootReceiver", "Boot completed, starting VolumeOverlayService")
+            val serviceIntent = Intent(context, VolumeOverlayService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
+
