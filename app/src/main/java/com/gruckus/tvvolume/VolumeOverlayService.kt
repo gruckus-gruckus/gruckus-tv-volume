349e6c45f9c6743ef045feff61878b01aeca6cbf
Gruckus <6582665+gruckus-gruckus@users.noreply.github.com>
2026-06-02 09:41:15 +1000

    minor logging

--- app/src/main/java/com/gruckus/tvvolume/VolumeOverlayService.kt
+++ app/src/main/java/com/gruckus/tvvolume/VolumeOverlayService.kt
@@ -154,7 +154,7 @@ class VolumeOverlayService : Service() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 onVolumeChanged()
                 val currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-                Log.d("LOGASDF", "Current volume on event: $currentVolume")
+                Log.d("Gruckus volume", "Current volume on event: $currentVolume")
             }
         }
         val filter = IntentFilter("android.media.VOLUME_CHANGED_ACTION")
