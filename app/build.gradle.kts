bc2cc1f2915c38ec18ccb42f84ad01b2c44410f4
Gruckus <6582665+gruckus-gruckus@users.noreply.github.com>
2026-06-02 12:14:47 +1000

    dpad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.gruckus.tvvolume"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
     }
 
